8d351a767f2500679897540814ceef785869a298
Nazar Vovk <nvovk.2004@gmail.com>
2024-12-15 19:29:16 +0200

    init electron

---
+++ electron/main.js
@@ -0,0 +1,34 @@
+// electron.js
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = 'http://localhost:5173';
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
